85bd4a5574f1b99e7c2877e0ba8b7b117353866c
belli <bellinna.uong@outlook.com>
2025-11-13 00:07:33 +0100

    ADd hover animation WIP

---
+++ app/src/react-app-env.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="react-scripts" />
+
+declare module '*.glb' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.fbx' {
+  const src: string;
+  export default src;
+}
+
